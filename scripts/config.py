1f4c45b405eb2c382be00ab1fa377cc8969195d7
Tyler Lindell <tyler@holographicinterfaces.com>
2023-04-19 08:26:01 -0700

    add remember mode setter

--- scripts/config.py
+++ scripts/config.py
@@ -144,3 +144,7 @@ class Config(metaclass=Singleton):
     def set_debug_mode(self, value: bool):
         """Set the debug mode value."""
         self.debug_mode = value
+
+    def set_remember_mode(self, value: bool):
+        """Set the remember mode value."""
+        self.remember_mode = value
